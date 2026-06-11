387f14f71a81f4712da36ac4eef33bc2708d248d
Joshua Smullen <158128671+JTSmullen@users.noreply.github.com>
2026-06-11 13:51:24 -0400

    Add JWT configuration beans to AutoAuthAutoConfiguration

--- AutoAuth-Processor/src/main/java/com/autoauth/processor/config/AutoAuthAutoConfiguration.java
+++ AutoAuth-Processor/src/main/java/com/autoauth/processor/config/AutoAuthAutoConfiguration.java
@@ -1,8 +1,34 @@
 package com.autoauth.processor.config;
 
+import com.autoauth.processor.jwt.JwtGenerator;
+import com.autoauth.processor.jwt.JwtKeyProvider;
+import com.autoauth.processor.jwt.JwtValidator;
+
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @AutoConfiguration
 @EnableConfigurationProperties(AutoAuthProperties.class)
-public class AutoAuthAutoConfiguration {}
+public class AutoAuthAutoConfiguration {
+
+  @Bean
+  @ConditionalOnMissingBean
+  public JwtKeyProvider (AutoAuthProperties properties) {
+    return new JwtKeyProvider(properties);
+  }
+
+  @Bean
+  @ConditionalOnMissingBean
+  public JwtGenerator jwtGenerator(JwtKeyProvider keyProvider, AutoAuthProperties properties) {
+    return new JwtGenerator(keyProvider, properties);
+  }
+
+  @Bean
+  @ConditionalOnMissingBean
+  public JwtValidator jwtValidator(JwtKeyProvider keyProvider) {
+    return new JwtValidator(keyProvider);
+  }
+  
+}
