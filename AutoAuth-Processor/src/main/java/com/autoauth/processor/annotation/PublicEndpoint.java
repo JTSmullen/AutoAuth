7c286ef4a43695f9292789f26ab5b5938dd7f021
Joshua Smullen <158128671+JTSmullen@users.noreply.github.com>
2026-06-11 10:12:15 -0400

    Add PublicEndpoint annotation for public access
    @PublicEndpoint annotation allows for a develop to mark a controller or method as "public" opening it up to the web without the need of a JWT for authorization. Useful for login / Register endpoints etc. where your request body won't have a token yet

---
+++ AutoAuth-Processor/src/main/java/com/autoauth/processor/annotation/PublicEndpoint.java
@@ -0,0 +1,10 @@
+package com.autoauth.processor.annotation;
+
+import java.lang.annotation.*;
+
+// Maker annotation to say whether a specific controller or method should be accessible without JWT
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface PublicEndpoint {}
