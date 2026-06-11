a77fe143fb3813cfe41e9a9e95a8953c7fe3e0ee
Joshua Smullen <158128671+JTSmullen@users.noreply.github.com>
2026-06-11 13:37:53 -0400

    Implement JwtGenerator for token generation
    Create the key with the user info + roles into it for easy extraction

---
+++ AutoAuth-Processor/src/main/java/com/autoauth/processor/jwt/JwtGenerator.java
@@ -0,0 +1,29 @@
+package com.autoauth.processor.jwt;
+
+import com.autoauth.processor.config.AutoAuthProperties;
+import com.autoauth.processor.model.AutoAuthUser;
+import io.jsonwebtoken.Jwts;
+
+import java.util.Date;
+
+public class JwtGenerator {
+  private final JwtKeyProvider keyProvider;
+  private final AutoAuthProperties properties;
+
+  public JwtGenerator (JwtKeyProvider, AutoAuthProperties properties) {
+    this.keyProvider = keyProvider;
+    this.properties = properties;
+  }
+
+  public String generateToken (AutoAuthUser user) {
+    long expirationTimeMillis = System.currentTimeMillis() + (properties.getExpirationMinutes() * 60 * 1000);
+
+    return Jwts.builder()
+      .subject(user.userId())
+      .claim("roles", user.roles())
+      .issuedAt(new Date())
+      .expiration(new Date(expirationTimeMillis))
+      .signWith(keyProvider.getKey(), Jwts.SIG.HS256)
+      .compact();
+  }
+}
