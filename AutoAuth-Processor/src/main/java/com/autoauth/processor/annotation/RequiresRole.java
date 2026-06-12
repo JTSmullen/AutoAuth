d428b43a516a7bb35e6d32ac166a709f493a4079
Smullen <smullenjt23@gcc.edu>
2026-06-12 13:25:16 -0400

    [Add] -> RequiresRole annotation

---
+++ AutoAuth-Processor/src/main/java/com/autoauth/processor/annotation/RequiresRole.java
@@ -0,0 +1,14 @@
+package com.autoauth.processor.annotation;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface RequiresRole {
+
+    String[] value();
+
+    boolean requireAll() default false;
+
+}
