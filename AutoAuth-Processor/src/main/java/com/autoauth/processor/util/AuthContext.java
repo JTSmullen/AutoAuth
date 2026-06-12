057b5d7e0c457fc3c29b3c0c22b2d4a05c6ab8ad
Smullen <smullenjt23@gcc.edu>
2026-06-12 10:52:30 -0400

    [Add] -> AuthContext to extract user Id and Roles from JWT token

---
+++ AutoAuth-Processor/src/main/java/com/autoauth/processor/util/AuthContext.java
@@ -0,0 +1,31 @@
+package com.autoauth.processor.util;
+
+import com.autoauth.processor.model.AutoAuthUser;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class AuthContext {
+    
+    public static Optional<AutoAuthUser> getCurrentUser() {
+        
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        
+        if (authentication != null && authentication.getPrincipal() instanceof AutoAuthUser) {
+            return Optional.of((AutoAuthUser) authentication.getPrincipal());
+        }
+        
+        return Optional.empty();
+        
+    }
+    
+    public static String getCurrentUserId() {
+        
+        return getCurrentUser()
+                .map(AutoAuthUser::userId)
+                .orElse(null);
+        
+    }
+    
+}
